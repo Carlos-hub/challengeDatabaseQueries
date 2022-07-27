c9a0af46567fa72fe8011e069af8c92dae6e128b
Carlos-hub <delaracarlosmiguel@gmail.com>
2022-07-27 08:06:19 -0300

    inserindo query pelo typeorm pelo raw query

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -1,5 +1,4 @@
-import { getRepository, Repository } from 'typeorm';
-
+import { getRepository, Repository, IsNull } from 'typeorm';
 import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from '../../dtos';
 import { User } from '../../entities/User';
 import { IUsersRepository } from '../IUsersRepository';
@@ -14,17 +13,26 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({
+      where: {
+        id: user_id,
+      },
+      relations: {
+        games: true
+      }
+    });
+    return user!;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return await this.repository.query(`SELECT * FROM users ORDER BY first_name`); 
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+
+    return await this.repository.query("select * from users where first_name ilike $1 and last_name ilike $2", [first_name, last_name]); // Complete usando raw query
   }
 }
